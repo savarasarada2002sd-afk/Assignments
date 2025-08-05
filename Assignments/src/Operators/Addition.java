cde0b17c1ecfdfc76fcdc6e50c5fc3d3930b2693
savar <savar@s-sarada2002>
2025-08-05 11:57:52 +0530

    done

---
+++ Assignments/src/Operators/Addition.java
@@ -0,0 +1,13 @@
+package Operators;
+
+public class Addition {
+	static int number1=12;
+	static int number2= 34;
+  static int c=number1+number2;
+	public static void main(String[] args) {
+		System.out.println("the result is "+c);
+		// TODO Auto-generated method stub
+
+	}
+
+}
